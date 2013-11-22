36c9a15433f46019365b137bdc3c0e3b7d7f913b
Nathan Marley <nathan.marley@gmail.com>
2013-11-21 21:04:24 -0800

    add their example

--- learnyounode/babysteps.js
+++ learnyounode/babysteps.js
@@ -1,3 +1,4 @@
+// my way
 function sum(nums) {
   var sum = 0;
   for (var i = 0; i < nums.length; ++i) {
@@ -7,3 +8,12 @@ function sum(nums) {
 }
 
 console.log(sum(process.argv.slice(2,process.argv.length)));
+
+
+// their way
+//
+// var result = 0
+// for (var i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+// console.log(result)
+
