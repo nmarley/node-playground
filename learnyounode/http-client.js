a8b3d5079865a212d88f80dba2c3c7365627d847
Nathan Marley <nathan.marley@gmail.com>
2013-11-24 08:43:10 -0800

    update http-client with official solution

--- learnyounode/http-client.js
+++ learnyounode/http-client.js
@@ -10,3 +10,11 @@ http.get(url, function(resp){
     console.log(data)
   })
 })
+
+// official solution
+//
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// })
