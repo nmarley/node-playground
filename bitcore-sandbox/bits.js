064be8273c1da34e4f1e4935b60b0b83fc5d355f
Nathan Marley <nathan.marley@gmail.com>
2014-05-12 19:15:26 -0700

    example bitcore BIP32 functionality

---
+++ bitcore-sandbox/bits.js
@@ -0,0 +1,23 @@
+// load the bitcore lib
+var bitcore = require('bitcore');
+
+// create a new BIP32 HD wallet
+var bip32 = new bitcore.BIP32();
+
+// log the master public and private keys for good measure (debugging)
+console.log(bip32.extendedPublicKeyString());
+console.log(bip32.extendedPrivateKeyString());
+
+// save off master private key string
+var masterPrivateKeyString = bip32.extendedPrivateKeyString();
+
+
+// load a new bip32 object from a pre-existing master private key string
+var duplicate_bip32 = new bitcore.BIP32(masterPrivateKeyString);
+
+// verify that they're the same (should print true)
+console.log(bip32.extendedPublicKeyString() == duplicate_bip32.extendedPublicKeyString());
+
+// BIP32 => HierarchicalKey
+
+// var key = bitcore.Key.generateSync();
