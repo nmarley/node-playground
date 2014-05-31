a6ffdb536ddc6319c0e710f4db4cfcf8d44f55ef
Nathan Marley <nathan.marley@gmail.com>
2014-05-31 14:37:49 -0700

    add moment UNIX offset snippet

---
+++ time-moment/unix-offset.js
@@ -0,0 +1,14 @@
+var moment = require('moment')
+
+// like the UNIX epoch, but in milliseconds
+console.log(moment().valueOf())
+
+// > moment().valueOf()
+// 1401572150478
+// > moment().valueOf()
+// 1401572152197
+// > moment().valueOf()
+// 1401572153100
+// > moment().valueOf()
+// 1401572154125
+
