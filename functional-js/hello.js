ed64856ef8bc4e813be23252e873d74b20672ae7
Nathan Marley <nathan.marley@gmail.com>
2014-06-04 16:30:54 -0700

    hello world in functional-js

---
+++ functional-js/hello.js
@@ -0,0 +1,8 @@
+// Write a function that takes an input string and returns it uppercased.
+// input: a String of random words (lorem ipsum).
+
+function upperCaser(input) {
+  return input.toUpperCase()
+}
+
+module.exports = upperCaser
