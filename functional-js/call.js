cd991a2f5dc3b9c563d3de042984237395fa5452
Nathan Marley <nathan.marley@gmail.com>
2014-06-05 18:27:29 -0700

    add duckCount official solution

--- functional-js/call.js
+++ functional-js/call.js
@@ -5,3 +5,15 @@ function duckCount() {
 }
 
 module.exports = duckCount
+
+
+// official solution
+/*
+function duckCount() {
+  return Array.prototype.slice.call(arguments).filter(function(obj) {
+    return Object.prototype.hasOwnProperty.call(obj, 'quack')
+  }).length
+}
+
+module.exports = duckCount
+*/
