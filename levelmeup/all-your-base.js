0ea8a3fba0b049147d8a47694beb165904cc0643
Nathan Marley <nathan.marley@gmail.com>
2014-06-20 13:29:01 -0700

    start level me up, all your base

---
+++ levelmeup/all-your-base.js
@@ -0,0 +1,6 @@
+
+// var base = process.argv[2]
+// var us = process.argv[3]
+// console.log("ALL YOUR " + base + " ARE BELONG TO " + us)
+
+console.log("ALL YOUR %s ARE BELONG TO %s", process.argv[2], process.argv[3])
