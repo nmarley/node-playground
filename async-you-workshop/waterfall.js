97010bf7c03a1e08938ed64f1bac9fe8ad88ea7a
Nathan Marley <nathan.marley@gmail.com>
2014-07-06 00:02:59 -0700

    use async.waterfall as per official solution

--- async-you-workshop/waterfall.js
+++ async-you-workshop/waterfall.js
@@ -1,20 +1,37 @@
 var fs = require('fs')
 var http = require('http')
+var async = require('async')
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
-  if (err)
-    throw err
-  var url = data.replace(/^\s+|\s+$/g, '')
-
-  var buff = []
-  http.get(url, function(resp) {
-    resp.on('data', function(chunk) {
-      buff.push(chunk)
-    }).on('error', function(err) {
-      console.error(err)
-      throw err
-    }).on('end', function() {
-      console.log(buff.join(''))
+async.waterfall([
+  function(callback) {
+    fs.readFile(process.argv[2], 'utf8', function(err, data) {
+      if (err)
+        return callback(err)
+      callback(null, data)
     })
-  })
-})
+  },
+
+  function(data, callback) {
+    var buff = []
+    var url = data.replace(/^\s+|\s+$/g, '')
+    http.get(url, function (resp) {
+      resp.on('data', function (chunk) {
+        buff.push(chunk)
+      }).on('error', function (err) {
+        callback(err)
+      }).on('end', function() {
+        console.log(buff.join(''))
+      })
+    }).on('error', function (err) {
+      callback(err)
+    })
+  }],
+
+  function (err, result) {
+    if (err)
+      console.error(err)
+    else
+      console.log(result)
+  }
+
+)
